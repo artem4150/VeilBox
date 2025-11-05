ca92dd9a4afbebb54183706612a91d4894541ae0
artem4150 <artemiygo9@gmail.com>
2025-11-06 03:09:07 +0500

    beta-3.1-fix tray

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"embed"
-	"github.com/google/uuid"
+	"log"
+
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/logger"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/windows"
 	"github.com/wailsapp/wails/v2/pkg/runtime"
-	"log"
 )
 
 //go:embed frontend/dist
@@ -25,11 +25,14 @@ func main() {
 		Assets:  assets,
 		Windows: &windows.Options{},
 		SingleInstanceLock: &options.SingleInstanceLock{
-			UniqueId: "veilbox-" + uuid.NewString(), // один и тот же ID в проде
-			OnSecondInstanceLaunch: func(data options.SecondInstanceData) {
+			UniqueId: "veilbox-single-instance",
+			OnSecondInstanceLaunch: func(options.SecondInstanceData) {
 				if app != nil && app.ctx != nil {
+					app.setWindowVisible(true)
 					runtime.Show(app.ctx)
-					runtime.EventsEmit(app.ctx, "tray:notification", "VeilBox уже запущён")
+					runtime.WindowShow(app.ctx)
+					runtime.WindowUnminimise(app.ctx)
+					runtime.EventsEmit(app.ctx, "tray:notification", "VeilBox restored from tray")
 				}
 			},
 		},
